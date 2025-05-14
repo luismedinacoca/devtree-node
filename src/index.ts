ff1dd7090bbd672d8fd7e3e8a43be2eee6e4dc72
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-14 17:43:32 -0300

    Section 04 - Lecture 016: Separando la creación del servidor en su propio archivo

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-//const express = require('express');   // CJS: Common JS
-import express from 'express'           // ESM: EcmaScript Modules
+import server from './server';
 
-//console.log(express)
-const app = express();
-
-//routing:
-app.get('/', (req, res) => {
-    //res.render()  
-    res.send('Hello world from Express / Typescript');
-})
-
-app.get('/users', (req, res) => {
-    //res.render()  
-    res.send('Hello world from Users');
-})
-
-
-//const port = 3000;
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-    console.log('Servidor funcionando en el port: ', port);
+server.listen(port, () => {
+    console.log('Servidor funcionando en el puerto: ', port);
 })
